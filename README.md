1fc5a83d00c89b45fc223e09ce3d0dd1710641e0
Lucca Bertoncini <luccabazzo@gmail.com>
2025-09-29 17:00:38 -0700

    main branch changes

--- README.md
+++ README.md
@@ -9,3 +9,4 @@ git clone git@github.com:luccabb/setup-macos.git
 - installs brew ([`brew`](https://brew.sh/))
 - installs miniconda ([`conda`](https://www.anaconda.com/docs/getting-started/miniconda/main))
 - sets up github: [`gh` CLI](https://github.com/cli/cli), creates and uploads an ssh key for you new mac to your git account
+- installs [starship](https://starship.rs/)
--- setup.sh
+++ setup.sh
@@ -48,3 +48,26 @@ gh config set git_protocol ssh
 git config --global pull.rebase false
 gh auth login --hostname github.com --git-protocol ssh --web
 
+# install starship
+curl -sS https://starship.rs/install.sh | sh -s -- -y
+eval "$(starship init zsh)"
+mkdir -p ~/.config && touch ~/.config/starship.toml
+cat > ~/.config/starship.toml <<'TOML'
+# Get editor completions based on the config schema
+"$schema" = 'https://starship.rs/config-schema.json'
+
+# Inserts a blank line between shell prompts
+add_newline = true
+
+# Replace the '❯' symbol in the prompt with '➜'
+[character] # The name of the module we are configuring is 'character'
+success_symbol = '[➜](bold green)' # The 'success_symbol' segment is being set to '➜' with the color 'bold green'
+
+# Disable the package module, hiding it from the prompt completely
+[package]
+disabled = true
+TOML
+
+grep -q 'starship init zsh' ~/.zshrc || echo 'eval "$(starship init zsh)"' >> ~/.zshrc
+. ~/.zshrc
+
